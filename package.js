b2e8c620cff24f73fb52fb6e0630910a96e9ec37
Yacine Merzouk <yacine@merzouk.ca>
2023-04-26 20:43:43 -0400

    1.0.15

--- package.js
+++ package.js
@@ -1,25 +1,18 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-url',
   summary: 'Fork of iron:url for Firestorm',
-  version: '1.0.10',
+  version: '1.0.15',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-url'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@2.7.3');
+  api.versionsFrom('METEOR@2.11.0');
 
   api.use('underscore');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.10');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.10');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.15');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.15');
 
   api.addFiles('lib/compiler.js');
   api.addFiles('lib/url.js');
-});
-
-Package.onTest(function (api) {
-  api.use('meteorhubdotnet:firestorm-iron-url');
-  api.use('tinytest');
-  api.use('test-helpers');
-  api.addFiles('test/url_test.js', ['client', 'server']);
 });
